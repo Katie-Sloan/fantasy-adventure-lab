81056d16c70e8f292418d4ea620e2f34478c7acb
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-17 12:23:14 +0000

    added map functionality to move

--- game_code/src/main/java/behaviour/Move.java
+++ game_code/src/main/java/behaviour/Move.java
@@ -4,9 +4,12 @@ import Player.PlayerController;
 public class Move implements IAction {
 
     PlayerController controller;
-
+    private int positionX;
+    private int positionY;
     public Move(PlayerController controller) {
         this.controller = controller;
+        positionX = 50;
+        positionY = 50;
     }
 
     public void setAction() {
@@ -16,4 +19,33 @@ public class Move implements IAction {
     public void stopAction() {
 
     }
+
+    public void moveUp() {
+        if(positionY == 100) return;
+        positionY++;
+    }
+    public void moveDown() {
+        if(positionY == 0) return;
+        positionY--;
+    }
+    public void moveRight() {
+        if(positionX == 100) return;
+        positionX++;
+    }
+    public void moveDown() {
+        if(positionX == 0) return;
+        positionX--;
+    }
 }
+//    x,y
+// 0,0 0,1 0,2
+// 1,0 1,1 1,2
+// 2,0 2,1 2,2
+//
+//
+//
+//
+//
+//
+//
+//
